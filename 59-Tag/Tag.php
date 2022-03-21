ae36646ab23a7b46eeeef26fb88f87ba1e9374fc
Annanek <81962176+Annanek@users.noreply.github.com>
2022-03-21 11:10:37 +0300

    Update Tag.php

--- 59-Tag/Tag.php
+++ 59-Tag/Tag.php
@@ -3,13 +3,38 @@
 class Tag 
 {
     private $name;
-    private $attrs;
+    private $attrs = [];
 
-    public function __construct($name, $attrs = []) 
+    public function __construct($name) 
     {
         $this->name = $name;
         $this->attrs = $attrs;
     }
+
+    public function setAttr($name, $value)
+    {
+        $this->attrs[$name] = $value;
+        return $this;
+    }
+
+    public function setAttrs($attrs)
+    {
+        foreach($attrs as $name => $value) {
+            $this->setAttr($name, $value);
+        }
+        return $this;
+    }
+
+
+
+    // public function removeAttr($name) //пока не понятно, как применять
+    // {
+    //     $attr = $this->attrs[$name];
+    //     if (isset($attr)) {
+    //         unset($attr);
+    //     }   
+    //     return $this;
+    // }
 
     public function open()
     {
@@ -31,7 +56,11 @@ class Tag
             $result = '';
 
             foreach ($attrs as $name => $value) {
-                $result .= " $name=\"$value\"";
+                if ($value === true) {
+                    $result .= " $name";
+                } else {
+                    $result .= " $name=\"$value\"";
+                } 
             }
 
             return $result;
