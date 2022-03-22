3d585ceb71fb312768927ca2629f69ce556f8c68
Annanek <81962176+Annanek@users.noreply.github.com>
2022-03-22 12:24:54 +0300

    Update Tag.php

--- 59-Tag/Tag.php
+++ 59-Tag/Tag.php
@@ -1,17 +1,68 @@
 <?php
 
-class Tag 
+require_once('iTag.php');
+
+class Tag implements iTag
 {
     private $name;
     private $attrs = [];
+    private $text = '';
 
     public function __construct($name) 
     {
         $this->name = $name;
-        $this->attrs = $attrs;
     }
 
-    public function setAttr($name, $value)
+    public function getName() 
+    {
+        return $this->name;
+    }
+
+    public function getText() 
+    {
+        return $this->text;
+    }
+
+    public function getAttrs() 
+    {
+        return $this->attrs;
+    }
+
+    public function getAttr($name)
+    {
+        if (isset($this->attrs[$name])) {
+            return $this->attrs[$name];
+        } else {
+            return null;
+        }
+    }
+
+    public function show()
+    {
+        $tag = $this->open() . $this->getText() . $this->close();
+        return $tag;
+    }
+
+    public function open()
+    {
+        $name = $this->name;
+        $attrsStr = $this->getAttrsStr($this->attrs);
+        return "<$name$attrsStr>";
+    }
+
+    public function close()
+    {
+        $name = $this->name;
+        return "</$name>";
+    }
+
+    public function setText($text)
+    {
+        $this->text = $text;
+        return $this;
+    }
+
+    public function setAttr($name, $value = true)
     {
         $this->attrs[$name] = $value;
         return $this;
@@ -25,28 +76,44 @@ class Tag
         return $this;
     }
 
-
-
-    // public function removeAttr($name) //пока не понятно, как применять
-    // {
-    //     $attr = $this->attrs[$name];
-    //     if (isset($attr)) {
-    //         unset($attr);
-    //     }   
-    //     return $this;
-    // }
-
-    public function open()
+    public function removeAttr($name) //пока не понятно, как применять
     {
-        $name = $this->name;
-        $attrsStr = $this->getAttrsStr($this->attrs);
-        return "<$name$attrsStr>";
+        $attr = $this->attrs[$name];
+        if (isset($attr)) {
+            unset($attr);
+        }   
+        return $this;
     }
 
-    public function close()
+    public function addClass($className) 
     {
-        $name = $this->name;
-        return "</$name>";
+        if (isset($this->attrs['class'])) {
+            $classNames = explode(' ', $this->attrs['class']);
+
+            if (!in_array($className, $classNames)) {
+                $classNames[] = $className;
+                $this->attrs['class'] = implode(' ', $classNames);
+            }
+        } else {
+            $this->attrs['class'] = $className;
+        }
+
+        return $this;
+
+    }
+
+    public function removeClass($className)
+    {
+        if (isset($this->attrs['class'])) {
+            $classNames = explode(' ', $this->attrs['class']);
+
+            if (in_array($className, $classNames)) {
+                $classNames = $this->removeElem($className, $classNames);
+                $this->attrs['class'] = implode(' ', $classNames);
+            }
+        }
+
+        return $this;
     }
 
     private function getAttrsStr($attrs)
@@ -67,5 +134,13 @@ class Tag
         } else {
             return '';
         }
+    }
+
+    private function removeElem($elem, $arr)
+    {
+        $key = array_search($elem, $arr);
+        array_splice($arr, $key, 1);
+
+        return $arr;
     }
 }
