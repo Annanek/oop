b0233703f0a3224e7a517e8a8168bbfc09f82a92
Annanek <81962176+Annanek@users.noreply.github.com>
2022-03-25 12:40:04 +0300

    Add files via upload

--- 69-Link/exercises/menu.php
+++ 69-Link/exercises/menu.php
@@ -1,1 +1,23 @@
+<?php
+require_once('../Link.php');
 
+define('ROOT', '/69-Link/exercises/');
+?>
+
+<h1>Menu</h1>
+
+<ul>
+<?php
+
+for ($i = 1; $i <= 5; $i++)
+{
+    echo '<li>';
+    echo (new Link)->setText("$i")->setAttr('href', ROOT . "$i.php")->show();
+    echo '</li>';
+}
+
+?>
+</ul>
+
+<?php
+echo $_SERVER['REQUEST_URI'];
